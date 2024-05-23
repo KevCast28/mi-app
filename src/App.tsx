b7ba87730271c7954a06d505b350de661a653599
KevCast28 <kevinsn524@gmail.com>
2024-05-23 13:45:07 -0500

    new file:   eslint.config.mjs 	modified:   package-lock.json 	modified:   package.json 	modified:   src/App.css 	modified:   src/App.tsx 	new file:   src/components/Footer.tsx 	new file:   src/components/Header.tsx 	new file:   src/components/SliderComponent.tsx 	new file:   src/img/slider1.png

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from '@mui/material/Button';
+import Avatar from '@mui/material/Avatar';
+import { deepOrange, deepPurple} from '@mui/material/colors';
+import { Box, Slider, Stack } from '@mui/material';
+import { VolumeDown, VolumeUp } from '@mui/icons-material';
+import Header from '../src/components/Header';
+import SliderComponent from './components/SliderComponent';
 
 function App() {
+  const [value, setValue] = React.useState<number>(30);
+
+  const handleChange = (event: Event, newValue: number | number[]) => {
+    setValue(newValue as number);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-header">
+        <Header />
+        <SliderComponent />
     </div>
   );
 }
